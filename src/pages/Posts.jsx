80a16aae690d09e96bd8b5b94f4c4e1a316741b3
maxchristensen <you@example.com>
2023-06-19 13:19:21 +1200

    posts fixed

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -15,18 +15,18 @@ const Posts = () => {
     useEffect(() => {
         axios.get(`${endpoint}`)
         .then((res) => {
-            console.log(res)
+            // console.log(res)
             setPosts(res.data)
             setLoading(false)
         })
         .catch((err) => console.log(err))
     })
 
-    const allPosts = ( {posts} ) => {
-        console.log({posts})
+    const AllPosts = ( {posts} ) => {
+        // console.log({posts})
         const mappedPosts = posts.map((post, index) => {
             return (
-                <div className="post-container">
+                <div className="post-container" key={post.slug + "-" + index}>
                     <h4 className='title'>{post.title.rendered}</h4>
                     <div dangerouslySetInnerHTML={{__html: post.excerpt.rendered}} />
                     <div>Key: {post.slug + "-" + index}</div>
@@ -37,7 +37,7 @@ const Posts = () => {
             )
         })
 
-        console.log(mappedPosts);
+        // console.log(mappedPosts);
 
         return (
             <>
@@ -53,7 +53,7 @@ const Posts = () => {
     <div className="container">
         <h2>Posts:</h2>
         <div className="homeCont">
-            {loading ? <p>Loading...</p> : <Post posts={posts} />}
+            {loading ? <p>Loading...</p> : <AllPosts posts={posts} />}
         </div>
 
     </div>
