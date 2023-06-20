c3b6c830375050e49b639f63e5bd10a2b22ee204
maxchristensen <you@example.com>
2023-06-20 14:00:01 +1200

    donations added to storefront

--- src/pages/Shop/Storefront.jsx
+++ src/pages/Shop/Storefront.jsx
@@ -6,20 +6,69 @@ const Storefront = () => {
     
     const productsUrl = import.meta.env.VITE_WC_PRODUCTS_URL
 
+    const [donations, setDonations] = useState(null)
     const [products, setProducts] = useState(null)
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         axios.get(`${productsUrl}`)
         .then((res) => {
-            console.log(res.data)
-            setProducts(res.data)
+            // console.log(res.data)
             setLoading(false)
+
+            let donationList =[]
+            let productsList = []
+
+            res.data.map((product) => {
+                if (product.categories[0].name === 'Donation'){
+                    donationList.push(product)
+                } else {
+                    productsList.push(product)
+                }
+            })
+
+            setDonations(sortDonations({donationList}))
+            setProducts(productsList)
         })
         .catch((err) => console.log(err))
     }, [])
 
-    const Products = ({ products }) => {
+    function sortDonations({donationList}) {
+     
+            const sortedDonations = donationList.sort((a, b) => {
+                let priceA = parseInt(a.prices.price)
+                let priceB = parseInt(b.prices.price)
+                if (priceA < priceB) return -1;
+                if (priceA > priceB) return 1;
+                return 0;
+              });
+
+
+
+            return sortedDonations;
+    }
+
+    const Donations = () => {
+        // console.log(donations);
+        const mappedDonations = donations.map((donation, index) => {
+            return (
+                <div className="product-container item-container" key={index}>
+                    <Link className="product-link" to={`/product/${donation.id}`} >
+                        <h4 className="name">{donation.name}</h4>
+                    </Link>
+                    <h3 className="name">${donation.prices.regular_price/100} {donation.prices.currency_code}</h3>
+                </div>
+                )
+        })
+
+            return(
+                <>
+                {mappedDonations}
+                </>
+            )
+    }
+
+    const Products = () => {
         // console.log({ products });
         const mappedProducts = products.map((product, index) => {
             function getFeaturedImage(product) {
@@ -29,14 +78,14 @@ const Storefront = () => {
                     return 'https://via.placeholder.com/150';
                 }
             }
+
             return (
             <div className="product-container item-container" key={index}>
                 <img className="product-Image" src={getFeaturedImage(product)} alt="Product Image" />
                 <Link className="product-link" to={`/product/${product.id}`} >
                     <h4 className="name">{product.name}</h4>
                 </Link>
-                <h3 className="name">${product.prices.price/100} {product.prices.currency_code}</h3>
-
+                <h3 className="name">${product.prices.regular_price/100} {product.prices.currency_code}</h3>
             </div>
             )
         })
@@ -55,7 +104,11 @@ const Storefront = () => {
         <div id="shop-page" className="container">
             <h2>Shop</h2>
             <div id="product-grid" className="grid-container">
-            {loading ? <p>Loading</p> : <Products products={products} />}
+            {loading ? <p>Loading</p> : <>
+            <Donations/>
+            <Products />
+            
+            </>}
             </div>
         </div>
         )
